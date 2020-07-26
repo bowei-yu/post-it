92c985cf329927c8f73730b3cba94d1fb16569d3
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2020-07-26 19:49:34 +0800

    like posts

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton';
+
 function PostCard({ post: {body, createdAt, id, username, likeCount, commentCount, likes} }) {
     
-    function likePost() {
-      console.log('Like post!');
-    }
-
-    function commentOnPost() {
-      console.log('Comment on post');
-    }
+    const { user } = useContext(AuthContext);
 
     return (
     <Card fluid>
@@ -28,15 +25,8 @@ function PostCard({ post: {body, createdAt, id, username, likeCount, commentCoun
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
-      <Button as='div' labelPosition='right' onClick={likePost}>
-        <Button basic color='red'>
-          <Icon name='heart'/>
-        </Button>
-        <Label basic color='red' pointing='left'>
-          {likeCount}
-        </Label>
-      </Button>
-      <Button as='div' labelPosition='right' onClick={commentOnPost}>
+        <LikeButton user={user} post={{ id, likes, likeCount }}/>
+      <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
         <Button basic color='teal'>
           <Icon name='comments'/>
         </Button>
@@ -44,6 +34,12 @@ function PostCard({ post: {body, createdAt, id, username, likeCount, commentCoun
           {commentCount}
         </Label>
       </Button>
+      {user && user.username === username && (
+        <Button as="div" color="google plus" 
+          floated="right" onClick={() => console.log('Delete post')}>
+          <Icon name="trash" style={{ margin: 0}}/>
+        </Button>
+      )}
       </Card.Content>
     </Card>
     );
